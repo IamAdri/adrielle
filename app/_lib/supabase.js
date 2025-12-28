5bf6a2b8eb5db630a306c2e466a7f32006e2fbeb
IamAdri <atoma304@gmail.com>
2025-12-28 19:13:18 +0200

    Move to .env sensitive data

--- app/_lib/supabase.js
+++ app/_lib/supabase.js
@@ -1,12 +1,12 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  "https://ydghorluedeqhuwnokqn.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlkZ2hvcmx1ZWRlcWh1d25va3FuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA3NjAxOTMsImV4cCI6MjA2NjMzNjE5M30.eeR-MGRRZivlr7KJD2KT1RxgJqzl7UY46SXcoxlrMHU",
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY,
   {
     realtime: {
       params: {
-        eventsPerSecond: 10, // optional throttle
+        eventsPerSecond: 10,
       },
     },
   }
